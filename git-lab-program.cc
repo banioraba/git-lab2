edf3812edc1a6809b58dc5c6170259b7a72a7247
AL-Husain Bani Oraba <hb407717@ohio.edu>
2020-09-03 14:09:56 -0400

    git lab prgram commit

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: <AL-Husain Ali Bani Oraba>
+ *        Date: <09/2/2020>
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+cout << "Hello Git!!" << endl;
+    /*add code*/
+    return 0;
+}// main
